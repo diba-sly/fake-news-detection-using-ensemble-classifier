fa1fd87a77a9aaa4ae1abe5bebd1f2d23a870c6f
diba-sly <d.salehiye@gmail.com>
2024-02-09 20:22:40 +0330

    add model 10

--- Scripts/Fake_News_Detection_Stacked_MLP.py
+++ Scripts/Fake_News_Detection_Stacked_MLP.py
@@ -9,9 +9,13 @@
 from sklearn.neural_network import MLPClassifier
 import time
 from sklearn.metrics import f1_score, accuracy_score, classification_report
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, Flatten
+import numpy as np
+from tensorflow.keras.layers import *
 
 
-# In[ ]:
+# In[2]:
 
 
 def build_mlp(X_train, y_train, X_test):
@@ -28,7 +32,7 @@ def build_mlp(X_train, y_train, X_test):
     return mlp_predictions
 
 
-# In[2]:
+# In[3]:
 
 
 def mlp_metrics(mlp_predictions, y_test):
@@ -38,6 +42,32 @@ def mlp_metrics(mlp_predictions, y_test):
     print("MLP F-score:", f_score)
     print("MLP Accuracy:", accuracy)
     return f_score, accuracy
+
+
+# In[4]:
+
+
+def build_mlp_with_embedding(X_train, y_train, X_test, embedding_matrix, vocab_size,embed_dim=150,input_length=304):    
+    model = Sequential()
+    
+    # Create and set the weights for the embedding layer
+    embedding_layer = Embedding(vocab_size, embed_dim, input_length=input_length, 
+                                 weights=[embedding_matrix], trainable=False)
+    
+    model.add(embedding_layer)
+    model.add(Dropout(0.1))
+    model.add(Flatten())
+    model.add(Dropout(0.1))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    
+    model.fit(X_train, y_train, epochs=5, batch_size=64)
+    
+    mlp_predictions = model.predict(X_test)
+    
+    return mlp_predictions
 
 
 # In[ ]:
